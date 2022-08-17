b698215656b2964ced97f2e81ea894598035e2b0
Bryan Chambers <bryches@gmail.com>
2022-08-16 20:21:35 -0400

    Setting up app outline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+//import { useEffect, useState } from 'react'
+
+import Company from './Company'
+// import Vehicle     from './Vehicle'
+// import Stop        from './Stop'
+
+import companies from './company_exclusions.json'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [routeList, setRouteList] = useState([])
+
+    // useEffect(() => {
+    //     const getRoutes = async () => {
+    //         const res  = await fetch(API + 'routes?filter[type]=0,1,2')
+    //         const data = await res.json()
+    //         setRouteList(data.data)
+    //     }
+    //     getRoutes()
+    // }, [])
+
+
+    // const [route, setRoute] = useState('CR-Providence')
+
+    // const handleRouteChange = (selected) => {
+    //     setRoute(selected)
+    // }
+
+    const results = companies
+
+    return (
+        <div className='App'>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Company</th>
+                            <th>Ticker</th>
+                            <th>Issues</th>
+                        </tr>
+
+                        <tr>
+                            <th>Search Input</th>
+                            <th>Search Input</th>
+                            <th>Issue Filters</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        { results.map((company) => {
+                            return (
+                                <Company
+                                    {...company}
+                                />
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Company.js
@@ -0,0 +1,16 @@
+//import './Company.css'
+//import { useEffect, useState } from 'react'
+
+
+function Company(props) {
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{props.ticker}</td>
+            <td>1234</td>
+        </tr>
+    )
+}
+
+export default Company
